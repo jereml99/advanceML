05f2ce58203a899fd955485f39132f7c318f21e4
jereml99 <ledwojeremi@gmail.com>
2024-04-24 14:49:52 +0200

    Refactor DataLoader in MyDataModule to use torch_geometric.loader

--- project3/datamodule.py
+++ project3/datamodule.py
@@ -3,9 +3,11 @@ import torch
 from torch.utils.data import random_split
 import torch.utils.data as data
 from torch_geometric.datasets import TUDataset
+from torch_geometric.loader import DataLoader
 
+FEATURE_DIM = 7
 
-class MyDataModule(L.LightningDataModule):
+class TUDataMoudle(L.LightningDataModule):
     def prepare_data(self):
         TUDataset(root="./data/", name="MUTAG")
 
@@ -19,10 +21,10 @@ class MyDataModule(L.LightningDataModule):
         )
 
     def train_dataloader(self):
-        return data.DataLoader(self.train_dataset)
+        return DataLoader(self.train_dataset, batch_size=100)
 
     def val_dataloader(self):
-        return data.DataLoader(self.validation_dataset)
+        return DataLoader(self.validation_dataset, batch_size=44)
 
     def test_dataloader(self):
-        return data.DataLoader(self.test_dataset)
+        return DataLoader(self.test_dataset)
